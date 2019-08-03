b605f61d31e10fb407ffdbd1cc719114545ba08d
Vincent Chow <vincent@gromm.co>
2019-08-03 16:22:45 +0800

    Create authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+/* eslint-disable require-atomic-updates */
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id, token });
+
+        if (!user) {
+            throw new Error();
+        }
+
+        req.token = token;
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: 'Please authenticate!' });
+    }
+};
+
+module.exports = auth;
--- models/user.js
+++ models/user.js
@@ -53,7 +53,7 @@ userSchema.methods.toJSON = function() {
 userSchema.methods.generateAuthToken = async function() {
     const user = this;
     const token = jwt.sign(
-        { _id: user._id.toString(), pin: user.pin },
+        { _id: user._id.toString() },
         process.env.JWT_SECRET
     );
 
