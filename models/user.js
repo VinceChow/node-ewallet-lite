e1e921c92d1f8270a0c9056e02526255c0720852
Vincent Chow <vincent@gromm.co>
2019-08-03 11:50:14 +0800

    Create user model

---
+++ models/user.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Invalid email');
+                }
+            }
+        },
+        pin: {
+            type: String,
+            required: true,
+            minlength: 6,
+            maxlength: 6
+        },
+        token: {
+            type: String,
+            required: true
+        },
+        balance: {
+            type: Number,
+            default: 0.0
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+// Remove sensitive information
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+
+    delete userObject.pin;
+    delete userObject.token;
+    
+    return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+    user.token = token;
+    await user.save();
+
+    return token;
+};
+
+// Has the plain text pin before saving
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('pin')) {
+        user.pin = await bcrypt.hash(user.pin, 8);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
