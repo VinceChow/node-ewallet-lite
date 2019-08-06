afbd42f502e05a2232a09bb92a22d7dbd68f93dc
Vincent Chow <vincent@gromm.co>
2019-08-06 14:15:16 +0800

    Integrate SendGrid and send welcome email

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const { ACCOUNT_STATUS } = require('../utils/constants/user');
+const {
+    sendWelcomeEmail,
+    sendCancelationEmail
+} = require('../utils/emails/account');
 
 const router = new express.Router();
 
@@ -9,6 +13,7 @@ router.post('/register', async (req, res) => {
     const user = new User(req.body);
     try {
         await user.save();
+        sendWelcomeEmail(user.email, user.name);
         const token = await user.generateAuthToken();
         res.status(201).send({ user, token });
     } catch (error) {
@@ -73,6 +78,7 @@ router.delete('/delete', auth, async (req, res) => {
     try {
         req.user.status = ACCOUNT_STATUS.DELETE;
         await req.user.save();
+        sendCancelationEmail(req.user.email, req.user.name);
         res.send('User account has been deleted.');
     } catch (err) {
         res.status(400).send(err);
